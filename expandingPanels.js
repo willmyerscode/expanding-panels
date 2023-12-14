81adb7bafb518bbe93da76bd08fc9429db0439fa
Abi Bacon <139892873+abibdevelopment@users.noreply.github.com>
2023-12-14 15:31:42 +0000

    Create expandingPanels.js

---
+++ expandingPanels.js
@@ -0,0 +1,193 @@
+/**
+* Expanding Panels
+* Copyright Will-Myers.com
+*/
+
+class ExpandingPanels {
+
+constructor (el) {
+   this.el = el;
+  
+   // Get Data Attributes
+   this.collection = this.el.getAttribute('data-source');
+   this.collection = this.collection.toLowerCase();
+   this.collection = this.collection.trim();
+
+   // Whole Panel Link
+   this.panelLink = this.el.getAttribute('data-link') || 'false';
+   this.panelLink = this.panelLink.toLowerCase();
+   this.panelLink = this.panelLink.trim();
+
+   // Open On Load
+   this.onLoad = this.el.getAttribute('data-open') || 'false';
+   this.onLoad = this.onLoad.toLowerCase();
+   this.onLoad = this.onLoad.trim();
+
+   // Find Action
+   this.action = this.el.getAttribute('data-action') || 'hover';
+   this.action = this.action.toLowerCase();
+   this.action = this.action.trim();
+  
+   this.collectionJSON = this.collection + '?format=json';
+   this.collectionData;
+
+   // Find Parent Section
+   this.parentSection = this.el.closest('.content');
+
+   
+  this.init();
+  
+}
+
+async init () {
+
+  this.collectionData = await this.getCollectionData();
+
+  this.buildExpandingPanels();
+
+  this.addHoverEventListener();
+
+  this.addPanelLink();
+
+  this.openOnLoad();
+
+}
+
+async getCollectionData() {
+     try {
+      const response = await fetch(this.collectionJSON);
+       console.log(response);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      this.data = await response.json();
+      this.data.items.forEach(item => {
+        console.log(item);
+        console.log(item.title);
+        this.title = item.title;
+        this.url = item.fullUrl;
+        this.image = item.assetUrl;
+        this.excerpt = item.excerpt;
+      });
+      return this.data; // Return the data so it can be used after await
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+}  
+  
+buildExpandingPanels() {  
+  let expandingPanel = document.createElement('div');
+  expandingPanel.classList.add('wm-grid');
+  this.el.append(expandingPanel);
+  let panel;
+  
+  this.data.items.forEach(item => {
+    panel = `<div class="wm-panel" style="background-image: url(` + item.assetUrl +`);">
+    <div class="vertical-title"><h4>` + item.title + `</h4></div>
+    <div class="wm-content-wrapper">
+    <div class="title"><h4>` + item.title + `</h4></div>
+    <div class="excerpt"><p>` + item.excerpt + `</p></div>
+    <a class="wm-button sqs-block-button-element--medium sqs-button-element--primary sqs-block-button-element" href="` + item.fullUrl + `">View More</a>
+    </div>
+    </div>`
+
+    
+    expandingPanel.insertAdjacentHTML("beforeEnd", panel);
+   
+      });
+
+     // Find Panels
+   this.panels = document.querySelectorAll('.wm-panel');
+   this.instancePanels = this.el.querySelectorAll('.wm-panel');
+
+  
+}
+
+addHoverEventListener() {
+  this.instancePanels.forEach((panel, index) => {
+    // Add hover event listener to each panel
+    panel.addEventListener('mouseover', () => {
+      // Add the 'panel-active' class when hovering
+      panel.classList.add('panel-active');
+      panel.classList.remove('panel-inactive');
+
+      // Remove 'panel-active' class from other panels of this instance
+      this.instancePanels.forEach((otherPanel, otherIndex) => {
+        if (otherIndex !== index) {
+          otherPanel.classList.remove('panel-active');
+          otherPanel.classList.add('panel-inactive');
+        }
+      });
+    });
+
+    // Remove the 'panel-active' class when not hovering
+    panel.addEventListener('mouseout', () => {
+      panel.classList.remove('panel-active');
+      panel.classList.add('panel-inactive');
+      // Remove 'panel-inactive' class from other panels of this instance
+      this.instancePanels.forEach((otherPanel) => {
+        otherPanel.classList.remove('panel-inactive');
+      });
+    });
+  });
+}
+
+addPanelLink(){
+ if (this.panelLink.includes("true")) {
+    this.panels.forEach((panel, index) => {
+      let wholePanelLink = `<a class="wm-panel-link" href="${this.data.items[index].fullUrl}"></a>`;
+      panel.insertAdjacentHTML("afterBegin", wholePanelLink);
+    });
+  }
+} 
+
+openOnLoad(){
+ if (this.panels.length > 0) {
+    this.firstPanel = this.panels[0];
+    this.lastPanel = this.panels[this.panels.length - 1];
+
+    if (this.onLoad.includes("first")) {
+      this.firstPanel.classList.add('panel-active');
+      this.instancePanels.forEach((panel, index) => {
+        if (index !== 0) {
+          panel.classList.add('panel-inactive');
+        } else {
+          panel.classList.remove('panel-inactive');
+        }
+      });
+    } else if (this.onLoad.includes("last")) {
+      this.lastPanel.classList.add('panel-active');
+      this.instancePanels.forEach((panel, index) => {
+        if (index !== this.instancePanels.length - 1) {
+          panel.classList.add('panel-inactive');
+        }
+
+      });
+    } else if (!isNaN(parseInt(this.onLoad))) {
+       this.instancePanels.forEach((panel, index) => {
+        if (index == parseInt(this.onLoad) - 1) {
+          panel.classList.add('panel-active');
+          panel.classList.remove('panel-inactive');
+        } else {
+          panel.classList.add('panel-inactive');
+          panel.classList.remove('panel-active');
+        }
+      });
+    }
+   
+  }
+  
+}
+
+}
+(function(){
+  // Find All Instances
+  let allInstances = document.querySelectorAll('[data-wm-plugin="expanding-panels"]');
+  
+  // Loop Through All Instances
+  for (let instance of allInstances) {
+    new ExpandingPanels(instance);
+  }
+}())
+
